f3f25396bcfee31f7217ec45c7f75a74b356fbc3
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-04-14 17:53:15 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,46 +13,46 @@
 
 from telethon import events, Button, custom
 import re, os
-from LEGENDX import PHOTO, xbot, BOT, VERSION
+from LEGENDX import Photo, xbot, VERSION
 from LEGENDX import bot
 @xbot.on(events.NewMessage(pattern=("/alive|/start")))
 async def awake(event):
-  LEGENDX = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {BOT}\n\n"
-  LEGENDX += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
-  LEGENDX += f"{BOT} VERSION : {VERSION} ʟᴀsᴛᴇsᴛ\n\n"
-  LEGENDX += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
-  LEGENDX += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
-  LEGENDX += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
-  LEGENDX += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
-  BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{BOT} 𝚁𝙴𝙿𝙾", "https://github.com/LEGENDX/LEGENDX")]]
+  DARKLONX = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {BOT}\n\n"
+  DARKLONX += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
+  DARKLONX += f"{BOT} VERSION : {VERSION} ʟᴀsᴛᴇsᴛ\n\n"
+  DARKLONX += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
+  DARKLONX += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
+  DARKLONX += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
+  DARKLONX += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
+  BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{BOT} 𝚁𝙴𝙿𝙾", "https://github.com/DARKLONX/DARKLONX")]]
   BUTTON += [[custom.Button.inline("𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝚈", data="LEGENDX")]]
   await xbot.send_file(event.chat_id, PHOTO, caption=LEGENDX,  buttons=BUTTON)
 
 
 
 
-@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"LEGENDX")))
+@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"DARKLONX")))
 async def callback_query_handler(event):
 # inline by LEGENDX and PROBOY22 🔥
-  PROBOYX = [[Button.url("REPO-LEGENDX", "https://github.com/LEGENDX/LEGENDX")]]
-  PROBOYX +=[[Button.url("DEPLOY-LEGENDX", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FLEGENDX%2Flegendpack&template=https%3A%2F%2Fgithub.com%2FLEGENDX%2Flegendpack")]]
-  PROBOYX +=[[Button.url("TUTORIAL", "https://youtu.be/rGCSSFPsS4Q"), Button.url("STRING-SESSION", "https://repl.it/@LEGENDX/LEGENDX#main.py")]]
+  PROBOYX = [[Button.url("REPO-DARKLONX", "https://github.com/DARKLONX/DARKLONXX")]]
+  PROBOYX +=[[Button.url("DEPLOY-DARKLONX", )]]
+  PROBOYX +=[[Button.url("TUTORIAL", ""), Button.url("STRING-SESSION", "https://repl.it/@LEGENDX22/LEGEND-Bot#main.py")]]
   PROBOYX +=[[Button.url("API_ID & HASH", "https://t.me/usetgxbot"), Button.url("REDIS", "https://redislabs.com")]]
-  PROBOYX +=[[Button.url("SUPPORT CHANNEL", "https://t.me/LEGENDXBOT_OFFICIAL"), Button.url("SUPPORT GROUP", "https://t.me/LEGENDX_USERBOT_SUPPORT")]]
+  PROBOYX +=[[Button.url("SUPPORT CHANNEL", "https://t.me/DARKLONXCHAT"), Button.url("SUPPORT GROUP", "https://t.me/DARKLONXCHAT")]]
   PROBOYX +=[[custom.Button.inline("ALIVE", data="PROBOY")]]
   await event.edit(text=f"𝙰𝙻𝙻 𝙳𝙴𝚃𝙰𝙸𝙻𝚂 𝙾𝙵 𝚁𝙴𝙿𝙾𝚂", buttons=PROBOYX)
 
 
-@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"PROBOY")))
+@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"DARKLONX")))
 async def callback_query_handler(event):
 # inline by LEGENDX and PROBOY22 🔥
-  LEGENDX = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {BOT}\n\n"
-  LEGENDX += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
-  LEGENDX += f"{BOT} OS : {VERSION} ʟᴀsᴛᴇsᴛ\n\n"
-  LEGENDX += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
-  LEGENDX += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
-  LEGENDX += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
-  LEGENDX += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
+  DARKLONX = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {BOT}\n\n"
+  DARKLONX += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
+  DARKLONX += f"{BOT} OS : {VERSION} ʟᴀsᴛᴇsᴛ\n\n"
+  DARKLONX += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
+  DARKLONX += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
+  DARKLONX += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
+  DARKLONX += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
   BUTTONS = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{BOT} 𝚁𝙴𝙿𝙾", "https://github.com/LEGENDX/LEGENDX")]]
   BUTTONS += [[custom.Button.inline("𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝚈", data="LEGENDX")]]
   await event.edit(text=LEGENDX, buttons=BUTTONS)
@@ -60,4 +60,4 @@ async def callback_query_handler(event):
 
 @xbot.on(events.NewMessage(pattern=("/repo|#repo")))
 async def repo(event):
-  await xbot.send_message(event.chat, "ʀᴇᴘᴏ ᴏғ ʟᴇɢᴇɴᴅ-ʙᴏᴛ", buttons=[[Button.url("⚜️ ʀᴇᴘᴏ ⚜️", "https://github.com/LEGENDX/LEGENDX")]])
+  await xbot.send_message(event.chat, "ʀᴇᴘᴏ ᴏғ ʟᴇɢᴇɴᴅ-ʙᴏᴛ", buttons=[[Button.url("⚜️ ʀᴇᴘᴏ ⚜️", "https://github.com/DARKLONX/DARKLONX")]])
